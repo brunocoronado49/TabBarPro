2b74bc2bf8ebbe10f5125ea379fce2cd336bf9ef
brunocoronado49 <jofrancoracor@gmail.com>
2021-10-01 12:50:05 -0500

    Initial Commit

---
+++ TabBarPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabBarPro
+//
+//  Created by Bruno Coronado on 01/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabBarPro/TabBarProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarProApp.swift
+//  TabBarPro
+//
+//  Created by Bruno Coronado on 01/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TabBarProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
